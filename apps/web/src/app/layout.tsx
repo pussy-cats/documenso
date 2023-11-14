3982692f32f8ea98a0fcb0abc51202b1316463df
amardikamahdi <amardikamahdi@gmail.com>
2023-11-14 20:11:41 +0700

    fix: change title

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -21,7 +21,7 @@ const fontInter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 const fontCaveat = Caveat({ subsets: ['latin'], variable: '--font-signature' });
 
 export const metadata = {
-  title: 'Documenso - The Open Source DocuSign Alternative',
+  title: 'E - Signature PT. PGP',
   description:
     'Join Documenso, the open signing infrastructure, and get a 10x better signing experience. Pricing starts at $30/mo. forever! Sign in now and enjoy a faster, smarter, and more beautiful document signing process. Integrates with your favorite tools, customizable, and expandable. Support our mission and become a part of our open-source community.',
   keywords:
@@ -29,7 +29,7 @@ export const metadata = {
   authors: { name: 'Documenso, Inc.' },
   robots: 'index, follow',
   openGraph: {
-    title: 'Documenso - The Open Source DocuSign Alternative',
+    title: 'E - Signature PT. PGP',
     description:
       'Join Documenso, the open signing infrastructure, and get a 10x better signing experience. Pricing starts at $30/mo. forever! Sign in now and enjoy a faster, smarter, and more beautiful document signing process. Integrates with your favorite tools, customizable, and expandable. Support our mission and become a part of our open-source community.',
     type: 'website',
